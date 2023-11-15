6ca3669176ea06b5dbaf0178256a5f06a367e436
bhagat-abhishek <inst.abhishek@gmail.com>
2023-11-16 03:23:33 +0530

    Ep 02 - Setup + Livewire + Tailwind

---
+++ app/Livewire/CreateCode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreateCode extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.create-code');
+    }
+}
---
+++ resources/views/livewire/create-code.blade.php
@@ -0,0 +1,3 @@
+<div>
+    here is my live wire component
+</div>
